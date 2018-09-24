970831224b171b7eddf09408a6bd82c180920f1a
Ilham Fadhilah <ilhamfi_2701yahoo.com>
2018-09-24 10:52:31 +0700

    Progress

--- halaman_akhir.php
+++ halaman_akhir.php
@@ -7,32 +7,40 @@ session_start();
 		<title>Data User</title>
 	</head>
 	<body>
-		<table border>
-			<tr>
-				<td colspan="2" align="center">
-					<img src="uploaded/<?php echo $_SESSION['foto']; ?>">
-				</td>
-			</tr>
-			<tr>
-				<th>Hobby User</th>
-				<th>Action</th>
-			</tr>
-			<?php 
-			$data = $_SESSION['hobby'];
-			foreach ($data as $value) {
-			?>
-			<tr>
-				<td><?php echo $value; ?></td>
-				<td>
-					<form action=" " method="post">
-						<input type="hidden" name="index" value="<?php echo $value; ?>">
-						<input type="submit" name="delete" value="Hapus">
-					</form>
-				</td>
-			</tr>
-			<?php
-			}
-			?>
-		</table>
+		<center>
+			<table border>
+				<tr>
+					<td colspan="2" align="center">
+						<img src="uploaded/<?php echo $_SESSION['foto']; ?>" width="200">
+					</td>
+				</tr>
+				<tr>
+					<th>Hobby User</th>
+					<th>Action</th>
+				</tr>
+				<?php 
+				$data = $_SESSION['hobby'];
+				foreach ($data as $value) {
+				?>
+				<tr>
+					<td><?php echo $value; ?></td>
+					<td>
+						<form action=" " method="post">
+							<input type="hidden" name="index" value="<?php echo $value; ?>">
+							<input type="submit" name="delete" value="Hapus">
+						</form>
+					</td>
+				</tr>
+				<?php
+				}
+				?>
+			</table>
+		</center>
 	</body>
 </html>
+<?php
+if (isset($_POST['delete']) && isset($_POST['index'])) {
+	$index_value = $_POST['index'];
+	$data = $_SESSION['hobby'];
+}
+?>
